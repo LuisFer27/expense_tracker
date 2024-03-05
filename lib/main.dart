8f9cc93a4440125afa5f7cc3fcdc40eef8691cac
luis <luisfernando.anemona@gmail.com>
2024-03-04 18:45:19 -0600

    ajustes 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 var kDarkColorScheme = ColorScheme.fromSeed(
     brightness: Brightness.dark,
-    seedColor: const Color.fromARGB(255, 5, 99, 125));
+    seedColor: const Color.fromARGB(255, 96, 59, 181));
 
 void main() {
   runApp(
